ba1b842e024560c9f1920820a605b450e3d76c75
PrimozGodec <p.godec9@gmail.com>
2016-05-30 22:10:57 +0200

    Code redesign and minor updates

--- orangecontrib/educational/widgets/owkmeans.py
+++ orangecontrib/educational/widgets/owkmeans.py
@@ -3,26 +3,10 @@ from Orange.widgets.widget import OWWidget
 from Orange.data import DiscreteVariable, ContinuousVariable, Table, Domain
 from Orange.widgets import gui, settings, highcharts
 import numpy as np
-from itertools import chain
 from .utils.kmeans import Kmeans
-from PyQt4.QtCore import pyqtSlot, QObject
+from PyQt4.QtCore import pyqtSlot
 from os import path
-
-
-def rgb_hash_brighter(hash, percent_brighter):
-    r, g, b = hex_to_rgb(hash)
-    brightness_to_add = 255 * percent_brighter / 100
-    r, g, b = r + brightness_to_add, g + brightness_to_add, b + brightness_to_add
-    return rgb_to_hex(tuple(min(v, 255) for v in (r, g, b)))
-
-def hex_to_rgb(value):
-    value = value.lstrip('#')
-    lv = len(value)
-    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
-
-
-def rgb_to_hex(rgb):
-    return '#%02x%02x%02x' % rgb
+from .utils.color_transform import rgb_hash_brighter
 
 
 class Scatterplot(highcharts.Highchart):
@@ -56,7 +40,7 @@ class Scatterplot(highcharts.Highchart):
             })
             """
 
-    def __init__(self, selection_callback, click_callback, drag_callback, **kwargs):
+    def __init__(self, click_callback, drag_callback, **kwargs):
 
         # read javascript for drag and drop
         with open(path.join(path.dirname(__file__), 'resources', 'draggable-points.js'), 'r') as f:
@@ -66,7 +50,6 @@ class Scatterplot(highcharts.Highchart):
                          bridge=self,
                          enable_select='',
                          chart_type='scatter',
-                         selection_callback=selection_callback,
                          chart_events_click=self.js_click_function,
                          plotOptions_series_point_events_drag=self.js_drag_function,
                          plotOptions_series_point_events_drop=self.js_drop_function,
@@ -77,7 +60,6 @@ class Scatterplot(highcharts.Highchart):
         self.click_callback = click_callback
         self.drag_callback = drag_callback
 
-
     @pyqtSlot(float, float)
     def chart_clicked(self, x, y):
         self.click_callback(x, y)
@@ -86,19 +68,16 @@ class Scatterplot(highcharts.Highchart):
     def point_dragged(self, index, x, y):
         print(index, x, y)
         self.drag_callback(index, x, y)
-        print('a')
 
     @pyqtSlot(int, float, float)
     def point_dropped(self, index, x, y):
         self.drag_callback(index, x, y)
-        print('b')
-
 
 
 class OWKmeans(OWWidget):
 
     name = "Educational k-Means"
-    description = "Widget demonstrates working of k-mans algorithm."
+    description = "Widget demonstrates working of k-means algorithm."
     icon = "icons/mywidget.svg"
     want_main_area = False
 
@@ -138,8 +117,8 @@ class OWKmeans(OWWidget):
                                 callback=self.restart,
                                 sendSelectedValue=True)
         gui.spin(self.optionsBox, self, 'numberOfClusters',
-                 minv=0, maxv=10, step=1, label='Number of centroids:', # how much to limit
-                 callback=self.cluster_change)
+                 minv=0, maxv=10, step=1, label='Number of centroids:',  # how much to limit
+                 callback=self.number_of_clusters_changed)
 
         # step and restart buttons
         gui.button(self.optionsBox, self, 'Step', callback=self.step)
@@ -150,32 +129,28 @@ class OWKmeans(OWWidget):
         gui.rubber(self.controlArea)
 
         # plot
-        self.scatter = Scatterplot(selection_callback=self.on_selection,
-                                   click_callback=self.graph_clicked,
+        self.scatter = Scatterplot(click_callback=self.graph_clicked,
                                    drag_callback=self.centroid_dropped,
                                    xAxis_gridLineWidth=0,
                                    yAxis_gridLineWidth=0,
                                    title_text='',
                                    tooltip_shared=False,
-                                   # In development, we can enable debug mode
-                                   # and get right-click-inspect and related
-                                   # console utils available:
-                                   debug=True)
+                                   debug=True)  # TODO: set false when end of development
         # Just render an empty chart so it shows a nice 'No data to display'
-        # warning
         self.scatter.chart()
         self.mainArea.layout().addWidget(self.scatter)
 
+        self.k_means = None
 
-    def merge_x_y(self):
+    def concat_x_y(self):
         attr_x, attr_y = self.data.domain[self.attr_x], self.data.domain[self.attr_y]
         cols = []
         for attr in (attr_x, attr_y):
             subset = self.data[:, attr]
             cols.append(subset.Y if subset.Y.size else subset.X)
-        X = np.column_stack(cols)
+        x = np.column_stack(cols)
         domain = Domain([attr_x, attr_y])
-        return Table(domain, X)
+        return Table(domain, x)
 
     def set_data(self, data):
         self.data = data
@@ -190,17 +165,17 @@ class OWKmeans(OWWidget):
 
         init_combos()
 
-        def setEmptyPlot():
+        def set_empty_plot():
             self.scatter.clear()
             self.optionsBox.setDisabled(True)
 
         # if data contains at least two continuous attributes
         if sum(True for var in data.domain.attributes if isinstance(var, ContinuousVariable)) <= 2:
             self.info.setText("Too few Continuous feature. Min 2 required")
-            setEmptyPlot()
+            set_empty_plot()
         elif data is None:
             self.info.setText("No data on input yet, waiting to get something.")
-            setEmptyPlot()
+            set_empty_plot()
         else:
             self.info.setText("")
             self.optionsBox.setDisabled(False)
@@ -209,12 +184,20 @@ class OWKmeans(OWWidget):
             self.restart()
 
     def restart(self):
-        self.k_means = Kmeans(self.merge_x_y())
+        self.k_means = Kmeans(self.concat_x_y())
         self.stepNo = 0
-        self.cluster_change()
+        self.number_of_clusters_changed()
+        self.replot()
+
+    def step(self):
+        self.stepNo += 1
+        if self.stepNo % 2 == 1:
+            self.k_means.step()
         self.replot()
 
     def replot(self):
+        colors = ['#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce',
+                  '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a']
 
         if self.data is None or not self.attr_x or not self.attr_y:
             return
@@ -224,35 +207,26 @@ class OWKmeans(OWWidget):
 
         options = dict(series=[])
 
-        colors = ['#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce',
-            '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'] # define some more colors
-
-        # if self.stepNo == 0:
-        #     X = self.merge_x_y().X
-        #     options['series'].append(dict(data=X, showInLegend=False))
-        # else:
+        # plot data points
         for i, points in enumerate(self.k_means.centroids_belonging_points):
             options['series'].append(dict(data=points,
                                           showInLegend=False,
                                           color=rgb_hash_brighter(colors[i % len(colors)], 30)))
 
-
+        # plot centroids
         options['series'].append(dict(data=[{'x': p[0],
                                              'y':p[1],
                                              'marker':{'fillColor': colors[i % len(colors)]}}
-            for i, p in enumerate(self.k_means.centroids_before
-                                  if self.stepNo % 2 == 1
-                                  else self.k_means.centroids)],
+                                            for i, p in enumerate(self.k_means.centroids_before
+                                                                  if self.stepNo % 2 == 1
+                                                                  else self.k_means.centroids)],
                                       draggableX=True if self.stepNo == 0 else False,
                                       draggableY=True if self.stepNo == 0 else False,
                                       showInLegend=False,
                                       marker=dict(symbol='diamond',
-                                                  radius=7)))
+                                                  radius=10)))
 
-
-        # Besides the options dict, Highcharts can also be passed keyword
-        # parameters, where each parameter is split on underscores in
-        # simulated object hierarchy. This works:
+        # highcharts parameters
         kwargs = dict(
             xAxis_title_text=attr_x.name,
             yAxis_title_text=attr_y.name,
@@ -269,26 +243,16 @@ class OWKmeans(OWWidget):
         if attr_y.is_discrete:
             kwargs['yAxis_categories'] = attr_y.values
 
-        # That's it, we can scatter our scatter by calling its chart method
-        # with the parameters we'd constructed
+        # plot
         self.scatter.chart(options, **kwargs)
 
-    def on_selection(self, indices):
-        pass
-
-    def cluster_change(self):
+    def number_of_clusters_changed(self):
         if self.k_means.k < self.numberOfClusters:
             for _ in range(self.numberOfClusters - self.k_means.k):
                 self.k_means.add_centroids()
         else:
             for _ in range(self.k_means.k - self.numberOfClusters):
                 self.k_means.delete_centroids()
-        self.replot()
-
-    def step(self):
-        self.stepNo += 1
-        if self.stepNo % 2 == 1:
-            self.k_means.step()
         self.replot()
 
     def graph_clicked(self, x, y):
@@ -299,4 +263,3 @@ class OWKmeans(OWWidget):
     def centroid_dropped(self, _index, x, y):
         self.k_means.move_centroid(_index, x, y)
         self.replot()
-
--- orangecontrib/educational/widgets/utils/kmeans.py
+++ orangecontrib/educational/widgets/utils/kmeans.py
@@ -1,21 +1,24 @@
 import Orange
 import numpy as np
 from Orange.distance import Euclidean
-from random import sample
 
 
 class Kmeans:
 
     def __init__(self, data, centroids=None, distance_metric=Orange.distance.Euclidean):
-        self.k = len(centroids) if centroids is not None else 0
         self.data = data
         self.centroids = np.array(centroids) if centroids is not None else np.empty((0, 2))
         self.centroids_before = None
         self.centroids_belonging_points = [None] * self.k
         self.distance_metric = distance_metric
-        # if len od data is less than number of centroids
-        if len(self.data) < len(self.centroids):
+
+        # if len od data is less than number of centroids take only len(data) centroids
+        if len(self.data) < self.k:
             self.centroids = self.centroids[:len(self.data)]
+
+    @property
+    def k(self):
+        return len(self.centroids)
 
     def step(self):
         self.centroids_before = np.copy(self.centroids)
@@ -36,16 +39,14 @@ class Kmeans:
     def add_centroids(self, points=None):
         if points is not None:
             self.centroids = np.vstack((self.centroids, np.array(points)))
-        else:
+        else:  # if no point provided add one centroid
             self.centroids = np.vstack((self.centroids, self.random_positioning()))
-        self.k = len(self.centroids)
         self.centroids_belonging_points = self.centroids_belonging_points + \
             [None] * (self.k - len(self.centroids_belonging_points))
         self.set_centroid_belonging()
 
     def delete_centroids(self):
         self.centroids = self.centroids[:-1]
-        self.k = len(self.centroids)
         self.centroids_belonging_points = self.centroids_belonging_points[:-1]
         self.set_centroid_belonging()
 
@@ -60,24 +61,3 @@ class Kmeans:
         for i in range(len(self.centroids)):
             c_points = d[closest_centroid == i]
             self.centroids_belonging_points[i] = c_points
-
-
-# if __name__ == "__main__":
-    # domain = Domain([ContinuousVariable("x"), ContinuousVariable("y")])
-    # table = Table(domain, np.array([[1, 1], [2, 2], [1, 2], [1.5, 1.5],
-    #                                 [5, 5], [5.5, 5], [4.9, 4.8], [5.5, 5.5],
-    #                                 [1, 6], [0, 6], [0.5, 6], [0.5, 7]]))
-    #
-    #
-    # kmeans = Kmeans(table, 3)
-    # plt.plot(kmeans.data.X[:,0], kmeans.data.X[:,1], "ro")
-    # plt.plot(kmeans.centroids[:,0], kmeans.centroids[:,1], "go")
-    # plt.savefig("kmeans.png")
-    # plt.clf()
-    #
-    # for i in range(5):
-    #     kmeans.step()
-    #     plt.plot(kmeans.data.X[:,0], kmeans.data.X[:,1], "ro")
-    #     plt.plot(kmeans.centroids[:,0], kmeans.centroids[:,1], "go")
-    #     plt.savefig("kmeans%d.png" % i)
-    #     plt.clf()
