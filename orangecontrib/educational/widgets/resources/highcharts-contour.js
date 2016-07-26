88a034fa22d36f9fcd79200cb870a6e1bb0a5dc1
PrimozGodec <p.godec9@gmail.com>
2016-07-26 18:45:04 +0200

    Fixed bug in highcharts-contour. On mac system bind function were missing.

--- orangecontrib/educational/widgets/resources/highcharts-contour.js
+++ orangecontrib/educational/widgets/resources/highcharts-contour.js
@@ -51,6 +51,30 @@ wrap(Highcharts.ColorAxis.prototype, 'setAxisSize', function (proceed) {
 
 Highcharts.Axis.prototype.drawCrosshair = function() {};
 
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                                 ? this
+                                 : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
 // The Heatmap series type
 seriesTypes.contour = extendClass(seriesTypes.heatmap, {
     type: 'contour',
