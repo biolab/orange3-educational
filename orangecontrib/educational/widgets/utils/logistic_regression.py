640674904d16982fd51a6911287213c0c51ff3bc
PrimozGodec <p.godec9@gmail.com>
2016-08-02 12:10:48 +0200

    Implemented step logistic regression.

---
+++ orangecontrib/educational/widgets/utils/logistic_regression.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+from Orange.classification import Learner, Model
+
+
+class LogisticRegression:
+
+    x = None
+    y = None
+    theta = None
+    domain = None
+
+    def __init__(self, alpha, theta=None, data=None):
+        self.alpha = alpha
+        self.set_data(data)
+        self.set_theta(theta)
+
+    def set_data(self, data):
+        if data is not None:
+            self.x = data.X
+            self.y = data.Y
+            self.domain = data.domain
+
+    def set_theta(self, theta):
+        self.theta = theta
+
+    @property
+    def model(self):
+        return LogisticRegressionModel(self.theta, self.domain)
+
+    def step(self):
+        grad = self.dj(self.theta)
+        self.theta -= self.alpha * grad
+
+    def j(self, theta):
+        """
+        Cost function for logistic regression
+        """
+        yh = self.g(self.x.dot(theta))
+        return -sum(np.log(self.y * yh + (1 - self.y) * (1 - yh)))
+
+    def dj(self, theta):
+        """
+        Gradient of the cost function with L2 regularization
+        """
+        return (self.g(self.x.dot(theta)) - self.y).dot(self.x)
+
+    @staticmethod
+    def g(z):
+        """
+        sigmoid function
+
+        Parameters
+        ----------
+        z : array_like
+            values to evaluate with function
+        """
+
+        # limit values in z to avoid log with 0 produced by values almost 0
+        z_mod = np.minimum(z, 100 * np.ones(len(z)))
+        z_mod = np.maximum(z_mod, -100 * np.ones(len(z)))
+
+        return 1.0 / (1 + np.exp(- z_mod))
+
+
+class LogisticRegressionModel(Model):
+
+    def __init__(self, theta, domain):
+        super().__init__(domain)
+        self.theta = theta
+
+    def predict_storage(self, data):
+        return LogisticRegression.g(data.X.dot(self.theta))
