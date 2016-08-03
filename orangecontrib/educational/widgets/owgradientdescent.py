ea60cfdb60310e6b0bfd5b3ae5a88a0e09e0034c
PrimozGodec <p.godec9@gmail.com>
2016-08-03 17:28:34 +0200

    Code refactor in OWGradientDescent.

--- orangecontrib/educational/widgets/owgradientdescent.py
+++ orangecontrib/educational/widgets/owgradientdescent.py
@@ -2,12 +2,11 @@ from os import path
 import time
 
 import numpy as np
-from Orange.widgets.utils import itemmodels
+from scipy.ndimage import gaussian_filter
 from PyQt4.QtCore import pyqtSlot, Qt, QThread, SIGNAL
 from PyQt4.QtGui import QSizePolicy, QPixmap, QColor, QIcon
-from scipy.interpolate import splev, splprep
-from scipy.ndimage import gaussian_filter
 
+from Orange.widgets.utils import itemmodels
 from Orange.classification import Model
 from Orange.data import Table, ContinuousVariable, Domain, DiscreteVariable
 from Orange.widgets import gui
@@ -27,11 +26,11 @@ class Scatterplot(highcharts.Highchart):
     * enables scroll-wheel zooming,
     * set callback functions for click (in empty chart), drag and drop
     * enables moving of centroids points
-    * include drag_drop_js script by highchart
+    * include drag_drop_js script by highcharts
     """
 
-    js_click_function = """/**/(function(event) {
-            window.pybridge.chart_clicked(event.xAxis[0].value, event.yAxis[0].value);
+    js_click_function = """/**/(function(e) {
+            window.pybridge.chart_clicked(e.xAxis[0].value, e.yAxis[0].value);
         })
         """
 
@@ -41,7 +40,9 @@ class Scatterplot(highcharts.Highchart):
     def __init__(self, click_callback, **kwargs):
 
         # read javascript for drag and drop
-        with open(path.join(path.dirname(__file__), 'resources', 'highcharts-contour.js'), 'r') as f:
+        with open(
+                path.join(path.dirname(__file__), 'resources',
+                          'highcharts-contour.js'), 'r') as f:
             contours_js = f.read()
 
         super().__init__(enable_zoom=True,
@@ -60,41 +61,57 @@ class Scatterplot(highcharts.Highchart):
 
     @pyqtSlot(float, float)
     def chart_clicked(self, x, y):
+        """
+        Function is called from javascript when click event happens
+        """
         self.click_callback(x, y)
 
-    def remove_series(self, id):
+    def remove_series(self, idx):
+        """
+        Function remove series with id idx
+        """
         self.evalJS("""
             series = chart.get('{id}');
             if (series != null)
                 series.remove(true);
-            """.format(id=id))
+            """.format(id=idx))
 
-    def remove_last_point(self, id):
+    def remove_last_point(self, idx):
+        """
+        Function remove last point from series with id idx
+        """
         self.evalJS("""
             series = chart.get('{id}');
             if (series != null)
                 series.removePoint(series.data.length - 1, true);
-            """.format(id=id))
+            """.format(id=idx))
 
     def add_series(self, series):
+        """
+        Function add series to the chart
+        """
         for i, s in enumerate(series):
             self.exposeObject('series%d' % i, series[i])
             self.evalJS("chart.addSeries(series%d, true);" % i)
 
-    def add_point_to_series(self, id, x, y):
+    def add_point_to_series(self, idx, x, y):
+        """
+        Function add point to the series with id idx
+        """
         self.evalJS("""
             series = chart.get('{id}');
             series.addPoint([{x}, {y}]);
-        """.format(id=id, x=x, y=y))
+        """.format(id=idx, x=x, y=y))
 
 
 class Autoplay(QThread):
     """
-    Class used for separated thread when using "Autoplay" for k-means
+    Class used for separated thread when using "Autoplay" for gradient descent
+
     Parameters
     ----------
-    owkmeans : OWKmeans
-        Instance of OWKmeans class
+    ow_gradient_descent : OWGradientDescent
+        Instance of OWGradientDescent class
     """
 
     def __init__(self, ow_gradient_descent):
@@ -109,16 +126,19 @@ class Autoplay(QThread):
         Stepping through the algorithm until converge or user interrupts
         """
         while (not self.ow_gradient_descent.learner.converged and
-                   self.ow_gradient_descent.auto_play_enabled):
+               self.ow_gradient_descent.auto_play_enabled):
             self.emit(SIGNAL('step()'))
             time.sleep(2 - self.ow_gradient_descent.auto_play_speed)
         self.emit(SIGNAL('stop_auto_play()'))
 
 
 class OWGradientDescent(OWWidget):
+    """
+    Gradient descent widget algorithm
+    """
 
     name = "Gradient Descent"
-    description = "Widget demonstrates shows the procedure of gradient descent."
+    description = "Widget shows the procedure of gradient descent."
     icon = "icons/InteractiveKMeans.svg"
     want_main_area = True
 
@@ -144,6 +164,10 @@ class OWGradientDescent(OWWidget):
     cost_grid = None
     grid_size = 15
     contour_color = "#aaaaaa"
+    min_x = None
+    max_x = None
+    min_y = None
+    max_y = None
 
     # data
     data = None
@@ -151,9 +175,13 @@ class OWGradientDescent(OWWidget):
 
     # autoplay
     auto_play_enabled = False
-    autoplay_button_text = ["Run", "Stop"]
+    auto_play_button_text = ["Run", "Stop"]
+    auto_play_thread = None
 
     class Warning(OWWidget.Warning):
+        """
+        Class used fro widget warnings.
+        """
         to_few_features = Msg("Too few Continuous feature. Min 2 required")
         no_class = Msg("No class provided or only one class variable")
 
@@ -183,68 +211,51 @@ class OWGradientDescent(OWWidget):
         self.cbx.setModel(self.x_var_model)
         self.cby.setModel(self.y_var_model)
 
+        # properties box
         self.properties_box = gui.widgetBox(self.controlArea, "Properties")
-        self.alpha_spin = gui.spin(widget=self.properties_box,
-                                   master=self,
-                                   callback=self.change_alpha,
-                                   value="alpha",
-                                   label="Learning rate: ",
-                                   minv=0.01,
-                                   maxv=1,
-                                   step=0.01,
-                                   spinType=float)
-        self.stochastic_checkbox = gui.checkBox(widget=self.properties_box,
-                                                master=self,
-                                                callback=self.change_stochastic,
-                                                value="stochastic",
-                                                label="Stochastic: ")
-        self.restart_button = gui.button(widget=self.properties_box,
-                                       master=self,
-                                       callback=self.restart,
-                                       label="Restart")
+        self.alpha_spin = gui.spin(
+            widget=self.properties_box, master=self, callback=self.change_alpha,
+            value="alpha", label="Learning rate: ",
+            minv=0.01, maxv=1, step=0.01, spinType=float)
+        self.stochastic_checkbox = gui.checkBox(
+            widget=self.properties_box, master=self,
+            callback=self.change_stochastic, value="stochastic",
+            label="Stochastic: ")
+        self.restart_button = gui.button(
+            widget=self.properties_box, master=self,
+            callback=self.restart, label="Restart")
 
-
+        # step box
         self.step_box = gui.widgetBox(self.controlArea, "Manually step through")
+        self.step_button = gui.button(
+            widget=self.step_box, master=self, callback=self.step, label="Step")
+        self.step_back_button = gui.button(
+            widget=self.step_box, master=self, callback=self.step_back,
+            label="Step back")
 
-        self.step_button = gui.button(widget=self.step_box,
-                                       master=self,
-                                       callback=self.step,
-                                       label="Step")
-        self.step_back_button = gui.button(widget=self.step_box,
-                                       master=self,
-                                       callback=self.step_back,
-                                       label="Step back")
-
+        # run box
         self.run_box = gui.widgetBox(self.controlArea, "Run")
         self.auto_play_button = gui.button(
-            self.run_box, self, self.autoplay_button_text[0],
-            callback=self.auto_play)
-        self.auto_play_speed_spinner = gui.hSlider(self.run_box,
-                                                   self,
-                                                   'auto_play_speed',
-                                                   minValue=0,
-                                                   maxValue=1.91,
-                                                   step=0.1,
-                                                   intOnly=False,
-                                                   createLabel=False,
-                                                   label='Speed:')
+            widget=self.run_box, master=self,
+            label=self.auto_play_button_text[0], callback=self.auto_play)
+        self.auto_play_speed_spinner = gui.hSlider(
+            widget=self.run_box, master=self, value='auto_play_speed',
+            minValue=0, maxValue=1.91, step=0.1,
+            intOnly=False, createLabel=False, label='Speed:')
 
         # graph in mainArea
-        self.scatter = Scatterplot(click_callback=self.set_theta,
+        self.scatter = Scatterplot(click_callback=self.change_theta,
                                    xAxis_gridLineWidth=0,
                                    yAxis_gridLineWidth=0,
                                    title_text='',
                                    tooltip_shared=False,
                                    debug=True)
-
+        # TODO: set false when end of development
         gui.rubber(self.controlArea)
 
-        # TODO: set false when end of development
         # Just render an empty chart so it shows a nice 'No data to display'
         self.scatter.chart()
         self.mainArea.layout().addWidget(self.scatter)
-
-        # set random learner
 
     def set_data(self, data):
         """
@@ -283,22 +294,21 @@ class OWGradientDescent(OWWidget):
         self.Warning.clear()
 
         # clear variables
-        self.xv = None
-        self.yv = None
         self.cost_grid = None
+
+        dd = data.domain
 
         if data is None or len(data) == 0:
             self.data = None
             reset_combos()
             self.set_empty_plot()
-        elif sum(True for var in data.domain.attributes
+        elif sum(True for var in dd.attributes
                  if isinstance(var, ContinuousVariable)) < 2:
             self.data = None
             reset_combos()
             self.Warning.to_few_features()
             self.set_empty_plot()
-        elif (data.domain.class_var is None or
-                      len(data.domain.class_var.values) < 2):
+        elif dd.class_var is None or len(dd.class_var.values) < 2:
             self.data = None
             reset_combos()
             self.Warning.no_class()
@@ -312,38 +322,72 @@ class OWGradientDescent(OWWidget):
             self.restart()
 
     def set_empty_plot(self):
+        """
+        Function render empty plot
+        """
         self.scatter.clear()
 
     def restart(self):
+        """
+        Function restarts the algorithm
+        """
         self.selected_data = self.select_data()
-        self.learner = self.default_learner(data=Normalize(self.selected_data),
-                                            alpha=self.alpha, stochastic=self.stochastic)
+        self.learner = self.default_learner(
+            data=Normalize(self.selected_data),
+            alpha=self.alpha, stochastic=self.stochastic)
         self.replot()
 
     def change_alpha(self):
+        """
+        Function changes alpha parameter of the alogrithm
+        """
         if self.learner is not None:
             self.learner.set_alpha(self.alpha)
 
     def change_stochastic(self):
+        """
+        Function changes switches between stochastic or usual algorithm
+        """
         if self.learner is not None:
             self.learner.stochastic = self.stochastic
 
+    def change_theta(self, x, y):
+        """
+        Function set new theta
+        """
+        if self.learner is not None:
+            self.learner.set_theta([x, y])
+            self.scatter.remove_series("path")
+            self.scatter.add_series([
+                dict(id="path", data=[[x, y]], showInLegend=False,
+                     type="scatter", lineWidth=1,
+                     marker=dict(enabled=True, radius=2))],)
+
     def step(self):
+        """
+        Function performs one step of the algorithm
+        """
         if self.data is None:
             return
         if self.learner.theta is None:
-            self.set_theta(np.random.uniform(self.min_x, self.max_x),
-                           np.random.uniform(self.min_y, self.max_y))
+            self.change_theta(np.random.uniform(self.min_x, self.max_x),
+                              np.random.uniform(self.min_y, self.max_y))
         self.learner.step()
         theta = self.learner.theta
         self.plot_point(theta[0], theta[1])
 
     def step_back(self):
+        """
+        Function performs step back
+        """
         if self.learner.step_no > 0:
             self.learner.step_back()
             self.scatter.remove_last_point("path")
 
     def plot_point(self, x, y):
+        """
+        Function add point to the path
+        """
         self.scatter.add_point_to_series("path", x, y)
 
     def replot(self):
@@ -352,9 +396,6 @@ class OWGradientDescent(OWWidget):
         """
         if self.data is None:
             return
-
-        attr_x = self.data.domain[self.attr_x]
-        attr_y = self.data.domain[self.attr_y]
 
         optimal_theta = self.learner.optimized()
         self.min_x = optimal_theta[0] - 5
@@ -367,7 +408,6 @@ class OWGradientDescent(OWWidget):
         # gradient and contour
         options['series'] += self.plot_gradient_and_contour(
             self.min_x, self.max_x, self.min_y, self.max_y)
-
 
         min_value = np.min(self.cost_grid)
         max_value = np.max(self.cost_grid)
@@ -399,10 +439,10 @@ class OWGradientDescent(OWWidget):
 
         self.scatter.chart(options, **kwargs)
 
-
     def plot_gradient_and_contour(self, x_from, x_to, y_from, y_to):
         """
         Function constructs series for gradient and contour
+
         Parameters
         ----------
         x_from : float
@@ -413,6 +453,7 @@ class OWGradientDescent(OWWidget):
             Min grid y value
         y_to : float
             Max grid y value
+
         Returns
         -------
         list
@@ -422,19 +463,19 @@ class OWGradientDescent(OWWidget):
         # grid for gradient
         x = np.linspace(x_from, x_to, self.grid_size)
         y = np.linspace(y_from, y_to, self.grid_size)
-        self.xv, self.yv = np.meshgrid(x, y)
-        thetas = np.column_stack((self.xv.flatten(), self.yv.flatten()))
+        xv, yv = np.meshgrid(x, y)
+        thetas = np.column_stack((xv.flatten(), yv.flatten()))
 
         # cost_values = np.vstack([self.learner.j(theta) for theta in thetas])
         cost_values = self.learner.j(thetas)
 
         # results
-        self.cost_grid = cost_values.reshape(self.xv.shape)
+        self.cost_grid = cost_values.reshape(xv.shape)
 
         blurred = self.blur_grid(self.cost_grid)
 
         # return self.plot_gradient(self.xv, self.yv, blurred) + \
-        return self.plot_contour()
+        return self.plot_contour(xv, yv, blurred)
 
     def plot_gradient(self, x, y, grid):
         """
@@ -449,6 +490,7 @@ class OWGradientDescent(OWWidget):
         """
         Function takes two selected columns from data table and merge them
         in new Orange.data.Table
+
         Returns
         -------
         Table
@@ -471,31 +513,20 @@ class OWGradientDescent(OWWidget):
 
         return Table(domain, x, y, self.data.Y[:, None])
 
-    def plot_contour(self):
+    def plot_contour(self, xv, yv, cost_grid):
         """
         Function constructs contour lines
         """
 
         contour = Contour(
-            self.xv, self.yv, self.blur_grid(self.cost_grid))
+            xv, yv, cost_grid)
         contour_lines = contour.contours(
-            np.linspace(np.min(self.cost_grid), np.max(self.cost_grid), 10))
+            np.linspace(np.min(cost_grid), np.max(cost_grid), 10))
 
         series = []
         count = 0
         for key, value in contour_lines.items():
             for line in value:
-                # if len(line) > 3:
-                #     # if less than degree interpolation fails
-                #     tck, u = splprep(
-                #         [list(x) for x in zip(*reversed(line))],
-                #         s=0.001, k=3,
-                #         per=(len(line)
-                #              if np.allclose(line[0], line[-1])
-                #              else 0))
-                #     new_int = np.arange(0, 1.01, 0.01)
-                #     interpol_line = np.array(splev(new_int, tck)).T.tolist()
-                # else:
                 interpol_line = line
 
                 series.append(dict(data=interpol_line,
@@ -512,17 +543,11 @@ class OWGradientDescent(OWWidget):
 
     @staticmethod
     def blur_grid(grid):
+        """
+        Function blur the grid, to make crossings smoother
+        """
         filtered = gaussian_filter(grid, sigma=1)
         return filtered
-
-    def set_theta(self, x, y):
-        if self.learner is not None:
-            self.learner.set_theta([x, y])
-            self.scatter.remove_series("path")
-            self.scatter.add_series([
-                dict(id="path", data=[[x, y]], showInLegend=False,
-                     type="scatter", lineWidth=1,
-                     marker=dict(enabled=True, radius=2))],)
 
     def auto_play(self):
         """
@@ -530,15 +555,15 @@ class OWGradientDescent(OWWidget):
         """
         self.auto_play_enabled = not self.auto_play_enabled
         self.auto_play_button.setText(
-            self.autoplay_button_text[self.auto_play_enabled])
+            self.auto_play_button_text[self.auto_play_enabled])
         if self.auto_play_enabled:
             self.disable_controls(self.auto_play_enabled)
-            self.autoPlayThread = Autoplay(self)
-            self.connect(self.autoPlayThread, SIGNAL("step()"), self.step)
+            self.auto_play_thread = Autoplay(self)
+            self.connect(self.auto_play_thread, SIGNAL("step()"), self.step)
             self.connect(
-                self.autoPlayThread, SIGNAL("stop_auto_play()"),
+                self.auto_play_thread, SIGNAL("stop_auto_play()"),
                 self.stop_auto_play)
-            self.autoPlayThread.start()
+            self.auto_play_thread.start()
         else:
             self.stop_auto_play()
 
@@ -549,11 +574,12 @@ class OWGradientDescent(OWWidget):
         self.auto_play_enabled = False
         self.disable_controls(self.auto_play_enabled)
         self.auto_play_button.setText(
-            self.autoplay_button_text[self.auto_play_enabled])
+            self.auto_play_button_text[self.auto_play_enabled])
 
     def disable_controls(self, disabled):
+        """
+        Function disable or enable all controls except those from run part
+        """
         self.step_box.setDisabled(disabled)
         self.options_box.setDisabled(disabled)
         self.properties_box.setDisabled(disabled)
-
-
