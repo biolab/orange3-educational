2d7dfc2a99300d5825fa8e5faf6ce711e76d1296
Primož Godec <p.godec9@gmail.com>
2018-12-18 14:23:53 +0100

    OW1ka test temporary skipped

--- orangecontrib/educational/widgets/tests/test_ow1ka.py
+++ orangecontrib/educational/widgets/tests/test_ow1ka.py
@@ -1,3 +1,5 @@
+import unittest
+
 from Orange.widgets.tests.base import WidgetTest
 from orangecontrib.educational.widgets.ow1ka import OW1ka
 
@@ -5,10 +7,16 @@ from orangecontrib.educational.widgets.ow1ka import OW1ka
 class TestOW1ka(WidgetTest):
     def setUp(self):
         self.widget = self.create_widget(OW1ka)
-        self.widget.combo.addItem('https://www.1ka.si/podatki/139234/A4228E24/')
+
+    @unittest.skip("Travis has problems calling outside APIs")
+    def test_output_data(self):
+        self.widget.combo.addItem(
+            'https://www.1ka.si/podatki/139234/A4228E24/')
         self.widget.load_url()
 
-    def test_output_data(self):
         self.widget.commit()
         output = self.get_output(self.widget.Outputs.data)
         self.assertEqual(len(output), 5)
+
+    def test_widget_load(self):
+        self.assertIsNotNone(self.widget)
