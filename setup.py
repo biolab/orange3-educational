d1989f4ebd877f652878558e77c3629ee4b6d1b5
Primož Godec <p.godec9@gmail.com>
2019-07-01 14:29:19 +0200

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 NAME = "Orange3-Educational"
 
-VERSION = "0.1.8"
+VERSION = "0.2.0"
 
 DESCRIPTION = "Orange Educational add-on for Orange data mining software package."
 LONG_DESCRIPTION = open(path.join(path.dirname(__file__), 'README.md')).read()
