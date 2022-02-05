f380efedddfd45bcd2d5405380cae333e33af492
janezd <janez.demsar@fri.uni-lj.si>
2022-02-05 17:29:07 +0100

    Polynomial Classification: Fix error when last class is empty

--- orangecontrib/educational/widgets/owpolynomialclassification.py
+++ orangecontrib/educational/widgets/owpolynomialclassification.py
@@ -206,7 +206,8 @@ class OWPolynomialClassification(OWBaseLearner):
             return
 
         self.data = data
-        non_empty = np.bincount(data.Y[np.isfinite(data.Y)].astype(int)) > 0
+        non_empty = np.bincount(data.Y[np.isfinite(data.Y)].astype(int),
+                                minlength=len(domain.class_var.values)) > 0
         values = np.array(domain.class_var.values)[non_empty]
         combo.addItems(values.tolist())
         self.var_model.set_domain(self.data.domain)
